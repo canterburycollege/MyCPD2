7f9b2421292a60c950c2406ce7369b056f3f3bee
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-05-10 11:43:12 -0300

    replace hard-coded paths

--- index.php
+++ index.php
@@ -10,12 +10,17 @@
  * controller method then outputs a view.
  */
 
+// define config vars
+define('BASEPATH', __DIR__ . DIRECTORY_SEPARATOR); // path to this directory
+define('DS', DIRECTORY_SEPARATOR); // window & linux ude different seperators
+define('SYSPATH', BASEPATH . 'system' . DS);
 
-require("system/BaseController.php");  
-require("system/BaseModel.php");
-require("system/View.php");
-require("system/ViewModel.php");
-require("system/Loader.php");
+
+require(SYSPATH . 'BaseController.php');  
+require(SYSPATH . 'BaseModel.php');
+require(SYSPATH . 'View.php');
+require(SYSPATH . 'ViewModel.php');
+require(SYSPATH . 'Loader.php');
 
 $loader = new Loader(); 
 $controller = $loader->createController(); 
