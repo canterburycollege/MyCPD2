53ea27eee4309e0ef824ab3904e342f6fa30229f
nfriend <nfriend@nathans-imac.cant-col.ac.uk>
2013-05-20 09:04:49 +0100

    Removed duplicate code.

--- views/hub/index.php
+++ views/hub/index.php
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
 <h1><?= $viewModel->get('heading1'); ?></h1>
 <p>-- insert content here --</p>
-=======
-<?php
-
-
-echo "<h1>MyCPD Hub</h1>
-<p>-- insert nav bar here --</p>
-<p>-- insert message of the day here --</p>
-<p>-- insert content here --</p>";
->>>>>>> Added Moodle chrome.
-=======
-<?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
-<h1><?= $viewModel->get('heading1'); ?></h1>
-<p>-- insert content here --</p>
->>>>>>> Changed CONFIGPATH and BASEURL constants for dev system.
