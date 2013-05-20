4d8d8ea71f0aad3d80bf6d21eef71fce6d4e74e9
nfriend <nfriend@nathans-imac.cant-col.ac.uk>
2013-05-20 08:44:38 +0100

    Added Moodle chrome.

--- views/hub/index.php
+++ views/hub/index.php
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
 <?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
 <h1><?= $viewModel->get('heading1'); ?></h1>
 <p>-- insert content here --</p>
+=======
+<?php
+
+
+echo "<h1>MyCPD Hub</h1>
+<p>-- insert nav bar here --</p>
+<p>-- insert message of the day here --</p>
+<p>-- insert content here --</p>";
+>>>>>>> Added Moodle chrome.
