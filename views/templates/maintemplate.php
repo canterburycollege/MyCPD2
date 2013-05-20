37d4ed33e5ca92313415166729a3db1a9be63006
nfriend <nfriend@nathans-imac.cant-col.ac.uk>
2013-05-20 08:45:18 +0100

    Changed URL to include BASEURL constant.

--- views/templates/maintemplate.php
+++ views/templates/maintemplate.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title><?php echo $viewModel->get('pageTitle'); ?></title>
-        <link rel="stylesheet" href="http://localhost/MyCPD2/assets/css/default.css" />
+        <link rel="stylesheet" href="http://webdev-04<?= BASEURL ?>assets/css/default.css" />
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" />
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
     </head>
