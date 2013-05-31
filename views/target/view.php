a64fab8477510d7ade10972bbd242b9e93de9a20
Nathan Friend <nfriend@nathans-imac.cant-col.ac.uk>
2013-05-31 11:36:17 +0100

    Moodle chrome added to targets

--- views/target/view.php
+++ views/target/view.php
@@ -1,3 +1,27 @@
+<?php
+//require_once('/srv/www/htdocs/moodle/config.php');
+//require_login();
+$strmymoodle = get_string('myhome');
+$userid = $USER->id;  // Owner of the page
+$context = get_context_instance(CONTEXT_USER, $USER->id);
+$header = "$SITE->shortname: $strmymoodle";
+$context = get_context_instance(CONTEXT_SYSTEM);  // So we even see non-sticky blocks
+print_r($USER);
+// Start setting up the page
+$params = array();
+$PAGE->set_context($context);
+$PAGE->set_url('/my/index.php', $params);
+$PAGE->set_pagelayout('mydashboard');
+$PAGE->set_pagetype('my-index');
+$PAGE->blocks->add_region('content');
+$PAGE->set_title($header);
+$PAGE->set_heading($header);
+
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->blocks_for_region('content'); 
+?>
 <script type="text/javascript" src="<?= BASEURL ?>/assets/js/DataTables/media/js/jquery.dataTables.js"></script>
 
 <script>
@@ -44,4 +68,13 @@
             </tbody>
         </table>
     </form>
+
+
+
+<?php
+echo $OUTPUT->footer();
+
+
+
+
 
