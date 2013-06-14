93f54fb077e800c4e9ec8aee20cc6f8b77a02c4b
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-06-14 12:28:01 -0300

    end of day fri 14/6

--- views/adminManager/createGroup.php
+++ views/adminManager/createGroup.php
@@ -1,1 +1,20 @@
+<?php $manager = $viewModel->get('manager'); ?>
 <h1>Create New Group Form</h1>
+<form method="POST" action="">
+    <table>
+        <tr>
+            <td><?= $manager->firstname ?></td>
+        </tr>
+        <tr>
+            <td><label>Description: </label>
+                <input type="text" name="description" value="" />
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <input type="submit" value="Submit" />
+                <input type="reset" value="Cancel" />
+            </td>
+        </tr>
+    </table>
+</form>
