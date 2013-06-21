354450e93d1720b805cbe6e8585fe405f457443a
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-06-21 12:22:54 -0300

    add $logged_in_user to adminManager controller

--- models/activity.php
+++ models/activity.php
@@ -1,25 +1,33 @@
 <?php
 
 require_once SYSPATH . 'DbConnectionRegistry.php';
+require_once SYSPATH . 'formHelper.php';
 
 class ActivityModel extends BaseModel {
-
     
-// @todo move to own helper file
-    private function html_select_options($options, $selected_id) {
-        $html = '';
-        foreach ($options as $option) {
-            if ($option->id == $selected_id) {
-                $html .= '<option id="' . $option->id . '" selected>';
-            } else {
-                $html .= '<option id="' . $option->id . '">';
-            }
-            $html .= $option->description . '</option>';
+    public function archive($logged_in_user) {
+        //@todo academic_year move to global constant
+        $academic_year = "2012/09/03 00:00:00";
+        $sql = "
+            SELECT  * 
+            FROM    v_activity 
+            WHERE   employee_id = '{$logged_in_user}' 
+                    AND planned_date < '{$academic_year}'
+                ";
+        $dbConn = DbConnectionRegistry::getInstance('mycpd');
+        $results = $dbConn->get_all($sql, 'OBJECT');
+        if (empty($results)) {
+            // initialize array to prevent php warning msg.
+            $results = Array();
         }
-        return $html;
+        $this->viewModel->set("pageTitle", "MyCPD Hub");
+        $this->viewModel->set("heading1", "Activities");
+        $this->viewModel->set("activities", $results);
+
+        return $this->viewModel;
     }
 
-    public function create() {
+    public function create($logged_in_user) {
         $this->viewModel->set("pageTitle", "MyCPD Hub");
         return $this->viewModel;
     }
@@ -42,7 +50,7 @@ class ActivityModel extends BaseModel {
      * @todo check whether only current targets required
      * or maybe use option group to display archived at bottom of list
      */
-    private function get_targets() {
+    private function get_targets($logged_in_user) {
         /* @todo add IN clause to select only given user's targets */
         $sql = "SELECT  id, title AS description 
                 FROM    target
@@ -53,7 +61,7 @@ class ActivityModel extends BaseModel {
         return $results;
     }
 
-    public function index() {
+    public function index($logged_in_user) {
         $this->viewModel->set("pageTitle", "MyCPD Hub");
         $this->viewModel->set("heading1", "Activities");
         return $this->viewModel;
@@ -74,11 +82,11 @@ class ActivityModel extends BaseModel {
 
         $targets = $this->get_targets();
         $selected_target = $activity->target_id;
-        $this->viewModel->set("target_options", $this->html_select_options($targets, $selected_target));
+        $this->viewModel->set("target_options", html_select_options($targets, $selected_target));
 
         $priority_types = $this->get_priority_types();
         $selected_priority = $activity->priority_type_id;
-        $this->viewModel->set("priority_options", $this->html_select_options($priority_types, $selected_priority));
+        $this->viewModel->set("priority_options", html_select_options($priority_types, $selected_priority));
 
         return $this->viewModel;
     }
@@ -93,28 +101,6 @@ class ActivityModel extends BaseModel {
             WHERE   employee_id = '{$logged_in_user}'
                     AND planned_date > '{$academic_year}' 
                     ";
-        $dbConn = DbConnectionRegistry::getInstance('mycpd');
-        $results = $dbConn->get_all($sql, 'OBJECT');
-        if (empty($results)) {
-            // initialize array to prevent php warning msg.
-            $results = Array();
-        }
-        $this->viewModel->set("pageTitle", "MyCPD Hub");
-        $this->viewModel->set("heading1", "Activities");
-        $this->viewModel->set("activities", $results);
-
-        return $this->viewModel;
-    }
-
-    public function archive($logged_in_user) {
-        //@todo academic_year move to global constant
-        $academic_year = "2012/09/03 00:00:00";
-        $sql = "
-            SELECT  * 
-            FROM    v_activity 
-            WHERE   employee_id = '{$logged_in_user}' 
-                    AND planned_date < '{$academic_year}'
-                ";
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $results = $dbConn->get_all($sql, 'OBJECT');
         if (empty($results)) {
