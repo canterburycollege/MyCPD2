bd696d83cbfdf82896c493efd8683e31f62f97e7
Nathan Friend <nfriend@nathans-imac.cant-col.ac.uk>
2013-06-21 12:58:12 +0100

    Report links and All staff target report added.

---
+++ views/report/allstaff.php
@@ -0,0 +1,61 @@
+<?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
+<h1><img src="<?= BASEURL . '/assets/pix/report.gif' ?>" alt="Report icon"><?= $viewModel->get('heading1'); ?></h1>
+<script type="text/javascript" src="<?= BASEURL ?>/assets/js/DataTables/media/js/jquery.dataTables.js"></script>
+
+<script>
+    $(document).ready(function() {
+        filter = $('#table_detail').dataTable({
+            "bJQueryUI": true,
+             "bPaginate": false,
+        });
+        //auto fill fiter from get for linking. 
+        filter.fnFilter('<?php echo $_GET['id']; ?>');
+       
+    });
+
+function confirmPost()
+{
+var agree=confirm("Are you sure you want to delete this target?");
+if (agree)
+return true ;
+else
+return false ;
+}
+
+</script>
+ 
+<?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
+<h2><img src="<?= BASEURL . '/assets/pix/target.gif' ?>" alt="Target icon">All Staff Targets</h2>
+<div id="div_activities">
+    <form>
+        <table id="table_detail">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Target tag</th>
+                    <th>Target title</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Date Due</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($viewModel->get('targets') as $row): ?>
+                    <tr>
+                        <td><?= $row->firstname ?> <?= $row->lastname ?></td>
+                        <td><a href="../../activity/view/<?= $row->id ?>"><?= $row->title ?></a></td>
+                        <td><?= $row->title_ext ?></td>
+                        <td><?= $row->description ?></td>
+                        <td><?= $row->status ?></td>
+                        <td><?= $row->target_date ?></td>
+                        <td><a href="../../target/update/<?= $row->id ?>">(Update)</a> </td>
+                        <td><a href="../../target/delete/<?= $row->id ?>" onClick="return confirmPost()">(Delete)</a> </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </form>
+    
+    <a href ="../../target/create">Create a new target</a>
