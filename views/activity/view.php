7e2186f821f241380d4ade203ad98a2d2dd2dd1c
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-06-21 08:30:15 -0300

    remove duplicate news from activity view

--- views/activity/view.php
+++ views/activity/view.php
@@ -10,10 +10,6 @@
     });
     
 </script>
-
-<?php foreach ($viewModel->get('news') as $row): ?>
- <div class="news"><?= $row->description?></div>
-<?php endforeach; ?>
  
 <?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
  <h1><img src="<?= BASEURL . '/assets/pix/plan2.png' ?>" alt="Target icon"><?= $viewModel->get('heading1'); ?></h1><a href="<?= BASEURL ?>activity/archive/">Archive</a> | Current
