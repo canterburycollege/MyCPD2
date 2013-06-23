ad932ca13ca55063ba5e5f60b88a5a94d3523475
ruskie <russell.hardy@gmail.com>
2013-06-23 22:04:54 +0100

    faculty & section wip

--- controllers/adminmanager.php
+++ controllers/adminmanager.php
@@ -4,40 +4,38 @@
  * Controller for manager admin, e.g. assigning moodle users as managers, 
  * creating groups and assigning users to these groups.
  * 
+ * Also for maintaining organisational structure i.e. faculties & sections. 
+ * @todo Consider moving organisational structure to it's own Controller
+ * 
  * Loads models and views required by manager
  */
 class AdminManagerController extends BaseController {
-    
+
     public function __construct($action, $urlValues) {
-        $this->authorisation();
-        
         parent::__construct($action, $urlValues);
-        
         //create the model object
         require("models/adminManager.php");
         $this->model = new AdminManagerModel();
+        // check that logged in user is authorised for this controller
+        $this->authorisation();
     }
-    
+
     /**
      * Get list of staff and format for use in jQuery autocomplete
      */
-    public function ajaxStaffList(){
+    public function ajaxStaffList() {
         $search_term = $_GET['term']; // this is search term
         $staff_list = $this->model->ajaxStaffList($search_term);
         echo json_encode($staff_list);
     }
-    
+
     /**
      * Check that logged-in user is in admin group
-     * 
-     * @todo - ad form & select from db
      */
-    public function authorisation(){
-        $USER = $_SESSION['USER'];
-        $logged_in_user = $USER->id;
-        $admin_users = array(2);
-        if(!in_array($logged_in_user,$admin_users)){
-            echo '<h3>Error: You [' . $USER->username . '] are not authorised to view this page</h3>';
+    public function authorisation() {
+        if (!$this->model->authorisation($_SESSION['USER'])) {
+            echo '<h3>Error: You [' . $_SESSION['USER']->username .
+            '] are not authorised to view this page</h3>';
             exit;
         }
     }
@@ -45,31 +43,29 @@ class AdminManagerController extends BaseController {
     /**
      * Create group to be used to assign new group to manager
      */
-    public function createGroup(){
+    public function createGroup() {
         $manager = $_GET['id'];
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $this->model->createGroup($manager);
             header('Location: ' . BASEURL . 'adminManager/viewGroups/' . $manager);
-        }
-        else {
+        } else {
             $this->view->output($this->model->createGroupForm($manager));
         }
     }
-    
+
     /**
      * Create group_detail, used to assign moodle users to a group
      */
-    public function createGroupDetail(){
+    public function createGroupDetail() {
         $group = $_GET['id'];
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $this->model->createGroupDetail($group);
             header('Location: ' . BASEURL . 'adminManager/viewGroupDetails/' . $group);
-        }
-        else {
+        } else {
             $this->view->output($this->model->createGroupDetailForm($group));
         }
     }
-    
+
     /**
      * Assign moodle user as manager
      */
@@ -78,13 +74,12 @@ class AdminManagerController extends BaseController {
             $this->model->createManager();
             $manager = $_POST['moodle_user_id'];
             header('Location: ' . BASEURL . 'adminManager/createGroup/' . $manager);
-        }
-        else {
+        } else {
             $this->view->output($this->model->createManagerForm());
         }
     }
-    
-    public function deleteGroup(){
+
+    public function deleteGroup() {
         $id = $_GET['id'];
         $this->model->deleteGroup($id);
     }
@@ -94,13 +89,12 @@ class AdminManagerController extends BaseController {
         $this->model->deleteManager($moodle_user_id);
         header('Location: ' . BASEURL . 'adminManager/viewManagers');
     }
-    
+
     //default method
-    protected function index()
-    {
+    protected function index() {
         header('Location: ' . BASEURL . 'adminManager/viewManagers');
     }
-    
+
     /**
      * Update existing group, including adding/removing users
      */
@@ -113,32 +107,79 @@ class AdminManagerController extends BaseController {
             $this->view->output($this->model->updateGroupForm($id));
         }
     }
-    
+
     public function updateManager() {
         $moodle_user_id = $_GET['id'];
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $this->model->updateManager($moodle_user_id);
             header('Location: ' . BASEURL . 'adminManager/viewManagers');
-        }
-        else {
+        } else {
             $this->view->output($this->model->updateManagerForm($moodle_user_id));
         }
     }
-    
-    public function viewGroups(){
+
+    public function viewGroups() {
         $manager = $_GET['id'];
         $this->view->output($this->model->viewGroups($manager));
     }
-    
-    public function viewGroupDetails(){
+
+    public function viewGroupDetails() {
         $group = $_GET['id'];
         $this->view->output($this->model->viewGroupDetails($group));
     }
 
-
-    public function viewManagers(){
+    public function viewManagers() {
         $this->view->output($this->model->viewManagers());
     }
+    
+    
+    /*************************************************************************
+     * Organisation i.e. Faculties & Sections
+     *************************************************************************/
+
+    public function createFaculty() {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $this->model->createFaculty();
+            header('Location: ' . BASEURL . 'adminManager/viewFaculties/');
+        } else {
+            $this->view->output($this->model->createFacultyForm());
+        }
+    }
+    
+    public function createSection() {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $this->model->createSection();
+            header('Location: ' . BASEURL . 'adminManager/viewSections/');
+        } else {
+            $this->view->output($this->model->createSectionForm());
+        }
+    }
+    
+    public function deleteFaculty(){
+        $id = $_GET['id'];
+    }
+    
+    public function deleteSection(){
+        $id = $_GET['id'];
+    }
+
+    public function updateFaculty(){
+        $id = $_GET['id'];
+    }
+    
+    public function updateSection(){
+        $id = $_GET['id'];
+    }
+    
+    public function viewFaculties(){
+        
+    }
+    
+    public function viewSections(){
+        $faculty = $_GET['id'];
+    }
+    
 }
+
 /* End of file adminManager.php */
 /* Location: ./adminManager.php */
--- models/adminManager.php
+++ models/adminManager.php
@@ -4,27 +4,41 @@ require_once SYSPATH . 'DbConnectionRegistry.php';
 require_once SYSPATH . 'formHelper.php';
 
 class AdminManagerModel extends BaseModel {
-    
+
     /**
      * get array of staff for populating autocomplete
      * 
      * @param string $search_term
      * @return array
      */
-    public function ajaxStaffList($search_term){
+    public function ajaxStaffList($search_term) {
         $sql = "
             SELECT  id, displayname AS label  
             FROM    v_staff
-            WHERE   displayname LIKE '%".$search_term."%'
+            WHERE   displayname LIKE '%" . $search_term . "%'
             ORDER BY 2";
-        
+
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $results = $dbConn->get_all($sql, 'ARRAY');
 
         return $results;
     }
-    
-    public function createGroup($manager){
+
+    public function authorisation($USER) {
+        $sql = "
+            SELECT  count(*) as row_count
+            FROM    v_staff
+            WHERE   id = '{$USER->id}'";
+        $dbConn = DbConnectionRegistry::getInstance('mycpd');
+        $results = $dbConn->get_all($sql, 'OBJECT');
+        if ($results[0]->row_count > 0) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function createGroup($manager) {
         $sql = "
             INSERT INTO manager_group (
                 manager,
@@ -36,25 +50,25 @@ class AdminManagerModel extends BaseModel {
                 '{$_POST['section']}',
                 '{$_POST['description']}'
                 )";
-                
+
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
-    public function createGroupForm($manager){
+
+    public function createGroupForm($manager) {
         $this->viewModel->set("pageTitle", "MyCPD Admin");
-        $this->viewModel->set('managerDetails',$this->getStaffDetails($manager));
-        $this->viewModel->set("section_options",html_select_options($this->getSectionOptions()));
+        $this->viewModel->set('managerDetails', $this->getStaffDetails($manager));
+        $this->viewModel->set("section_options", html_select_options($this->getSectionOptions()));
         return $this->viewModel;
     }
-    
+
     /**
      * @todo Refactor to accept multiple inserts from same form
      * @param type $group
      */
-    public function createGroupDetail($group){
+    public function createGroupDetail($group) {
         $moodle_user_id = $_POST['moodle_user_id'];
-        
+
         $sql = "
             INSERT INTO manager_group_detail (
                 manager_group,
@@ -64,21 +78,21 @@ class AdminManagerModel extends BaseModel {
                 '{$group}',
                 '{$moodle_user_id}'
                 )";
-                
+
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
-    public function createGroupDetailForm($group){
-        
+
+    public function createGroupDetailForm($group) {
+
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         return $this->viewModel;
     }
-    
-    public function createManager(){
+
+    public function createManager() {
         $moodle_user_id = $_POST['moodle_user_id'];
         $description = $_POST['description'];
-        
+
         $sql = "INSERT INTO manager (
                     moodle_user_id,
                     description
@@ -91,32 +105,32 @@ class AdminManagerModel extends BaseModel {
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
-    public function createManagerForm(){
+
+    public function createManagerForm() {
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         return $this->viewModel;
     }
 
-    public function deleteGroup($id){
+    public function deleteGroup($id) {
         $sql = "DELETE FROM manager_group WHERE id ='{$id}'";
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
-    public function deleteManager($moodle_user_id){
+
+    public function deleteManager($moodle_user_id) {
         $sql = "
             DELETE FROM manager 
             WHERE moodle_user_id = '{$moodle_user_id}'";
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
+
     /**
      * return an array of options to be used in html select
      * 
      * @return array
      */
-    private function getSectionOptions(){
+    private function getSectionOptions() {
         $sql = "
             SELECT  s.id, 
                     CONCAT(s.description,' (',f.description,')') AS description
@@ -130,7 +144,7 @@ class AdminManagerModel extends BaseModel {
             // initialize array to prevent php warning msg.
             $results = Array();
         }
-        
+
         return $results;
     }
 
@@ -139,13 +153,13 @@ class AdminManagerModel extends BaseModel {
      * 
      * @param integer $moodle_user_id
      */
-    public function getStaffDetails($moodle_user_id){
+    public function getStaffDetails($moodle_user_id) {
         $sql = "
             SELECT  *
             FROM    v_staff
             WHERE   id = {$moodle_user_id}";
-            
-            $dbConn = DbConnectionRegistry::getInstance('mycpd');
+
+        $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $results = $dbConn->get_all($sql, 'OBJECT');
         if (empty($results)) {
             // initialize array to prevent php warning msg.
@@ -154,8 +168,8 @@ class AdminManagerModel extends BaseModel {
 
         return $results[0];
     }
-    
-    public function updateGroup($id){
+
+    public function updateGroup($id) {
         $section_id = $_POST['section_id'];
         $description = $_POST['description'];
         $sql = "
@@ -164,10 +178,10 @@ class AdminManagerModel extends BaseModel {
                     description = '{$description}'
             WHERE   id = '{$id}'";
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
-        $dbConn->execute($sql);    
+        $dbConn->execute($sql);
     }
-    
-    public function updateGroupForm($id){
+
+    public function updateGroupForm($id) {
         $sql = "
             SELECT  g.description,
                     g.section AS section_id,
@@ -179,15 +193,14 @@ class AdminManagerModel extends BaseModel {
             FROM    manager_group g
             WHERE   g.id = '{$id}'";
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
-        $results = $dbConn->get_all($sql, 'OBJECT'); 
+        $results = $dbConn->get_all($sql, 'OBJECT');
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         $this->viewModel->set("group", $results[0]);
-        $this->viewModel->set("section_options", 
-                html_select_options($this->getSectionOptions(), $results[0]->section_id));
+        $this->viewModel->set("section_options", html_select_options($this->getSectionOptions(), $results[0]->section_id));
         return $this->viewModel;
     }
 
-    public function updateManager($moodle_user_id){
+    public function updateManager($moodle_user_id) {
         $description = $_POST['description'];
         $sql = "
             UPDATE  manager 
@@ -197,8 +210,8 @@ class AdminManagerModel extends BaseModel {
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $dbConn->execute($sql);
     }
-    
-    public function updateManagerForm($moodle_user_id){
+
+    public function updateManagerForm($moodle_user_id) {
         $sql = "
             SELECT  m.moodle_user_id,
                     CONCAT(u.firstname,' ',u.lastname) as display_name,
@@ -208,15 +221,15 @@ class AdminManagerModel extends BaseModel {
                         ON u.id = m.moodle_user_id
             WHERE   m.moodle_user_id = {$moodle_user_id}
             ";
-            
+
         $dbConn = DbConnectionRegistry::getInstance('host');
-        $results = $dbConn->get_all($sql, 'OBJECT');   
+        $results = $dbConn->get_all($sql, 'OBJECT');
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         $this->viewModel->set("manager", $results[0]);
         return $this->viewModel;
     }
-    
-    public function viewGroupDetails($group){
+
+    public function viewGroupDetails($group) {
         // sql to get staff rows
         $sql1 = "
             SELECT  s.displayname
@@ -224,7 +237,7 @@ class AdminManagerModel extends BaseModel {
                     JOIN v_staff s
                         ON d.moodle_user_id = s.id
             WHERE   d.manager_group = {$group}";
-            
+
         // sql to get group row
         $sql2 = "
             SELECT  m.displayname as managerName,
@@ -233,7 +246,7 @@ class AdminManagerModel extends BaseModel {
                     JOIN v_staff m
                         ON g.manager = m.id
             WHERE   g.id = {$group}";
-            
+
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $staffRows = $dbConn->get_all($sql1, 'OBJECT');
         $groupRow = $dbConn->get_all($sql2, 'OBJECT');
@@ -245,16 +258,16 @@ class AdminManagerModel extends BaseModel {
             // initialize array to prevent php warning msg.
             $groupRow = Array();
         }
-        
+
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         $this->viewModel->set("staffRows", $staffRows);
         $this->viewModel->set("groupRow", $groupRow[0]);
-        $this->viewModel->set("group_id",$group);
+        $this->viewModel->set("group_id", $group);
 
         return $this->viewModel;
     }
-    
-    public function viewGroups($manager){
+
+    public function viewGroups($manager) {
         $sql = "
             SELECT  mg.id,
                     (
@@ -272,20 +285,20 @@ class AdminManagerModel extends BaseModel {
                     ) AS num_of_staff
             FROM    manager_group mg
             WHERE   mg.manager = '{$manager}'";
-            
+
         $dbConn = DbConnectionRegistry::getInstance('mycpd');
         $results = $dbConn->get_all($sql, 'OBJECT');
         if (empty($results)) {
             // initialize array to prevent php warning msg.
             $results = Array();
         }
-        
+
         $this->viewModel->set("pageTitle", "MyCPD Admin");
         $this->viewModel->set("groups", $results);
         // get manager name from moodle
         $this->viewModel->set("manager", $this->getStaffDetails($manager));
 
-        return $this->viewModel;            
+        return $this->viewModel;
     }
 
     public function viewManagers() {
@@ -309,5 +322,43 @@ class AdminManagerModel extends BaseModel {
 
         return $this->viewModel;
     }
+    
+    /*************************************************************************
+     * Organisation i.e. Faculties & Sections
+     *************************************************************************/
+    
+    public function createFaculty(){
+        $sql = "
+            INSERT INTO faculty (
+                description,
+                manager
+                )
+            VALUES (
+                '{$_POST['description']}',
+                '{$_POST['manager']}'
+            )";
+                
+        $dbConn = DbConnectionRegistry::getInstance('mycpd');
+        $dbConn->execute($sql);
+    }
+    
+    public function createSection(){
+        $sql = "
+            INSERT INTO section (
+                faculty,
+                description,
+                manager
+                )
+            VALUES (
+                '{$_POST['faculty']}',
+                '{$_POST['description']}',
+                '{$_POST['manager']}'
+            )";
+                
+        $dbConn = DbConnectionRegistry::getInstance('mycpd');
+        $dbConn->execute($sql);
+    }
+
 }
+
 ?>
