7efbd6b313bae4e78c9246bcf8e937933c81f842
ruskie <russell.hardy@gmail.com>
2013-06-28 23:54:37 +0100

    Update formHelper.php
    fix merge conflict

--- system/formHelper.php
+++ system/formHelper.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /**
@@ -23,29 +22,3 @@ function html_select_options($options, $selected_id = NULL) {
 }
 
 ?>
-=======
-<?php
-
-/**
- * Function to wrap an array of options in html tags, to be used inside of html
- * select. A parameter can be used to select a default option.
- * 
- * @param array $options
- * @param string $selected_id Default selected option
- * @return string html option list
- */
-function html_select_options($options, $selected_id = NULL) {
-    $html = '';
-    foreach ($options as $option) {
-        if ($option->id == $selected_id) {
-            $html .= '<option value="' . $option->id . '" selected>';
-        } else {
-            $html .= '<option value="' . $option->id . '">';
-        }
-        $html .= $option->description . '</option>';
-    }
-    return $html;
-}
-
-?>
->>>>>>> 9a5315a2928711a3abae245e216bf98f99ffc18a
