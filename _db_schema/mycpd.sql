ba6b204f46c6c0e90e95253784bdc38eca07bd0e
Russell Hardy <r.hardy@canterburycollege.ac.uk>
2013-06-28 10:28:53 -0300

    update schema

--- _db_schema/mycpd.sql
+++ _db_schema/mycpd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 28, 2013 at 01:27 PM
+-- Generation Time: Jun 28, 2013 at 01:28 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.4.3
 
