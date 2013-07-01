3c7b592f713536ca14662d5cb7e91d1379a260ab
Nathan Friend <nfriend@nathans-imac.cant-col.ac.uk>
2013-07-01 15:30:35 +0100

    Add nav bar links

--- views/templates/nav_bar.php
+++ views/templates/nav_bar.php
@@ -4,8 +4,9 @@
             <a href="<?= BASEURL ?>hub/view/">Hub</a> 
             | <a href="<?= BASEURL ?>target/view/">Targets</a>
             | <a href="<?= BASEURL ?>activity/view/">Activities</a>
-            | <a href="<?= BASEURL ?>report/view/">Reports</a> 
             | <a href="<?= BASEURL ?>mandatory/view/">Mandatory & Contractual Training</a>
+                        | <a href="<?= BASEURL ?>report/view/">Reports</a>
+                        | <a href="<?= BASEURL ?>admin/viewManagers">Admin</a> 
         </td>
     </tr>
 </table>
--- views/templates/nav_bar_admin.php
+++ views/templates/nav_bar_admin.php
@@ -3,7 +3,8 @@
         <td class='layout'>
             <a href="<?= BASEURL ?>admin/viewManagers/">Maintain Managers</a> | 
             <a href="<?= BASEURL ?>admin/viewSections/">Maintain Sections</a> |
-            <a href="<?= BASEURL ?>admin/viewFaculties/">Maintain Faculties</a>
+            <a href="<?= BASEURL ?>admin/viewFaculties/">Maintain Faculties</a> |
+            <a href="<?= BASEURL ?>">Back to MyCPD hub</a>
         </td>
     </tr>
 </table>
