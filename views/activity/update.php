b4c40bdfd444dc130dcfa8117a28c5a72324a8f4
Nathan Friend <nfriend@nathans-imac.cant-col.ac.uk>
2013-07-02 16:31:06 +0100

    Change jQuery script load order.

--- views/activity/update.php
+++ views/activity/update.php
@@ -1,13 +1,11 @@
+<script type="text/javascript" src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+<script type="text/javascript" src="<?= BASEURL ?>assets/js/raty/lib/jquery.raty.min.js"></script>
+<script type="text/javascript" src="<?= BASEURL . '/assets/js/tiny_mce/tiny_mce.js' ?>"></script>
 <?php
 $activity = $viewModel->get('activity');
 $target_options = $viewModel->get('target_options');
 $priority_options = $viewModel->get('priority_options');
 ?>
-
-<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
-<script type="text/javascript" src="<?= BASEURL ?>assets/js/raty/lib/jquery.raty.min.js"></script>
-<script type="text/javascript" src="<?= BASEURL . '/assets/js/tiny_mce/tiny_mce.js' ?>"></script>
-
 
 <script type="text/javascript">
 
@@ -21,68 +19,71 @@ $priority_options = $viewModel->get('priority_options');
     });
 
     $(function() {
-        $("#planned_date").datepicker({dateFormat: 'yy-mm-dd'});
-        $("#completed_date").datepicker({dateFormat: 'yy-mm-dd'});
+        $("#planned_date").datepicker({dateFormat: 'dd/mm/yy'});
+        $("#completed_date").datepicker({dateFormat: 'dd/mm/yy'});
         $('#star').raty({ 
             scoreName : 'rating',
             path : '<?= BASEURL ?>assets/js/raty/lib/img' ,
             cancel : true,
-            score : <?= $activity->rating ?>
+            score : '<?= $activity->rating ?>'
         });
     });
 
 </script>
 
 
-
-
-
-
-<h1>Update Activity</h1>
-<form method="post" action="">
+<?php include_once TEMPLATEPATH . 'nav_bar.php'; ?>
+<h1><img src="<?= BASEURL . '/assets/pix/plan2.png' ?>" alt="Activity icon"><?= $viewModel->get('heading1'); ?></h1>
+<form id="standard" method="post" action="">
     <table>
         <tbody>
             <tr>
-                <td>Target this CPD addresses: </td>
+                <td><label>Target this CPD addresses: </td>
                 <td><select name="target_id">
                         <?= $target_options ?>
                     </select>
-                </td>
+                </label></td>
             </tr>
             <tr>
-                <td>Title of CPD activity/event: </td>
-                <td><textarea name="title" cols="80" rows="5" ><?= $activity->title ?></textarea></td>
+                <td><label>Title of CPD activity/event: </td>
+                <td><textarea name="title" cols="80" rows="5" ><?= $activity->title ?></textarea></label></td>
             </tr>
             <tr>
-                <td>Intended learning outcomes for teacher: </td>
-                <td><textarea name="learning_outcomes" cols="80" rows="5" ><?= $activity->learning_outcomes ?></textarea></td>
+                <td><label>Intended learning outcomes for teacher: </td>
+                <td><textarea name="learning_outcomes" cols="80" rows="5" ><?= $activity->learning_outcomes ?></textarea></label></td>
             </tr>
             <tr>
-                <td>Intended impact on student outcomes: </td>
-                <td><textarea name="impact" cols="80" rows="5" ><?= $activity->impact ?></textarea></td>
+                <td><label>Intended impact on student outcomes: </td>
+                <td><textarea name="impact" cols="80" rows="5" ><?= $activity->impact ?></textarea></label></td>
             </tr>
             <tr>
-                <td>Priority level: </td>
+                <td><label>Priority level: </td>
                 <td><select name="priority_type_id">
                         <?= $priority_options ?>
                     </select>
-                </td>
+                </label></td>
             </tr>
             <tr>
-                <td>Target date: </td>
-                <td><input name="planned_date" id="planned_date" value="<?= $activity->planned_date ?>" /></td>
+                <td><label>Target date: </td>
+                <td><input name="planned_date" id="planned_date" value="<?= $activity->planned_date ?>" /></label></td>
             </tr>
             <tr>
-                <td>Completed? </td>
-                <td><input name="completed_date" id="completed_date" value="<?= $activity->completed_date ?>" /></td>
+                <td><label>Completed? </td>
+                <td><input name="completed_date" id="completed_date" value="<?= $activity->completed_date ?>" /></label></td>
             </tr>
+                         <tr>
+                <td><label>Evaluation notes: </td>
+                <td><textarea name="evaluation_notes" cols="80" rows="5" ><?= $activity->evaluation_notes ?></textarea></label></td>
+            </tr>
+            
             <tr>
-                <td>Rating: </td>
-                <td><div id="star"></div></td>
+                <td><label>Rating: </td>
+                <td><div id="star"></div></label></td>
             </tr>
+
         </tbody>
     </table>
     <br/>
-    <input type="submit" value="Submit" />
+    <input type="submit" value="Update Activity" />
     <input type="reset" value="Cancel" />
 </form>
