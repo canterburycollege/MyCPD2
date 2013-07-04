d59305bb674f6c60e07e794e015c9e2b029bd675
Nathan Friend <nfriend@lt82-dcm.cant-col.ac.uk>
2013-07-04 14:47:09 +0100

    Changed js base url

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
 // global variables, such as paths etc. used in javascript
 
 //var BASE_URL = 'http://localhost/moodle/MyCPD/';
-var BASE_URL = 'http://localhost/moodle/MyCPD/';
+var BASE_URL = 'http://webdev-04.cant-col.ac.uk/moodle/MyCPD/';
